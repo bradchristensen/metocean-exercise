d5365f74a017762638583315d42d5ed58010783b
Brad Christensen <brad@christensen.co.nz>
2016-04-16 15:05:50 +1200

    Fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ mkdir build
 mv * build
 tar -czf metocean.tgz build
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp christensen_co_nz.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/packages
+sshpass -e scp metocean.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/packages
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/scripts/deployMetOcean.sh
